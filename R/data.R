dc9e767e324cf40e5d28c125424d8473ca84312c
OlivierGranacher <olivier.granacher@gmail.com>
2021-12-06 19:56:57 +0100

    colorVectorPotline dataset

--- R/data.R
+++ R/data.R
@@ -1,13 +1,12 @@
-#' Prices of 50,000 round cut diamonds.
+#' Potline Colors
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A vector containing named colors from STJN potline - See getcolors vignette for
+#' details
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#'
+#' @format A named vector containing 10 colors
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
 #'   ...
 #' }
-#' @source \url{http://www.diamondse.info/}
-"diamonds"
+#'
+"colorVectorPotline"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,5 +4,9 @@ colorVectorPotline <- c(brown = "#752D2D", brownRed = "#D40A03", greyDark = "#96
                         beige = "#A5ACBF", pinkDark = "#BFB1CF", greyLight = "#8D7086",
                         greyRed = "#793E44", purpleLight = "#E8C1C3", pinkLight = "#C09192"
 )
+# stored in data/ for user access
 usethis::use_data(colorVectorPotline,
                   overwrite = T)
+
+# Can also be accessed internally with customColors::colorVectorPotline
+# https://coolbutuseless.github.io/2018/12/10/r-packages-internal-and-external-data/
