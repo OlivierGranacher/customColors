5dcd1c67a761abb00db9039f0bbcf97ff14b4337
OlivierGranacher <olivier.granacher@gmail.com>
2021-12-11 17:06:36 +0100

    interpolatePalette function

--- R/colorTools.R
+++ R/colorTools.R
@@ -18,3 +18,27 @@ extractColors <- function(..., colorVector = customColors::colorVectorPotline) {
 
   return(colorVector[selection])
 }
+
+#' interpolatePalette
+#'
+#'  Create a function to generate set of n colors from a color palette.
+#'
+#' @param paletteList list of named color vectors. Default to customColors::potlinePalettes
+#' @param paletteName name of palette in palette list.  Default to 'redGrey'
+#' @param reverse if TRUE reverse he colors
+#' @param ... passed on to colorRampPalette.
+#'
+#' @return function
+#' @export
+#'
+#' @examples interpolatePalette()(10)
+#'
+interpolatePalette <- function(paletteList = customColors::potlinePalettes,
+                               paletteName = "redGrey",
+                               reverse = F,
+                               ...) {
+
+    colr <- paletteList[[paletteName]]
+    if (reverse) colr <- rev(colr)
+    grDevices::colorRampPalette(colr, ...)
+}
