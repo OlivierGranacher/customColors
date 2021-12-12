45b668bf903f0446661a672bccd87adf1c0f77a7
OlivierGranacher <olivier.granacher@gmail.com>
2021-12-12 16:27:30 +0100

    Added testing

---
+++ tests/testthat/test-colorTools.R
@@ -0,0 +1,10 @@
+
+## colorVectorPotline contains at least 2 colors
+testthat::test_that("colorVectorPotline contains at least 2 colors", {
+  testthat::expect_gte(length(colorVectorPotline), 2)
+})
+
+## interpolatePalette function: tests that defaut palette exists
+testthat::test_that("Default palette for interpolation exists", {
+  testthat::expect_equal(names(potlinePalettes["redGrey"]), "redGrey")
+})
